f248193a37217b7c45fd986da8b87759511926bb
Brazencobra <elxan.memmedli2003@gmail.com>
2022-11-01 19:53:33 +0400

    First commit

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task1
+            //int[] numbers = { 13, 18, 3, 34 };
+            //int minimumnum = numbers[0];
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (minimumnum > numbers[i])
+            //    {
+            //        minimumnum = numbers[i];
+            //    }
+
+            //}
+            //Console.WriteLine("En kiçik element:" + minimumnum);
+            #endregion
+
+            #region Task2
+            int[] numbers = { 1, 13, 5, 77, };
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] < 10)
+                {
+                    sum++;
+
+                }
+            }
+            Console.WriteLine("Verilmiş numunede " + sum + " reqem var");
+
+            #endregion
+        }
+    }
+}
